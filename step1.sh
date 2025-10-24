bf6e08dc075c37d769eed15ca03c54481efa1b07
Harris Kirk <cjtkirk@protonmail.com>
2025-10-24 19:23:30 -0400

    Add sh to create github pub key

---
+++ step1.sh
@@ -0,0 +1,8 @@
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
+
+sudo apt install git
+git config --global user.name "Harris Kirk"
+git config --global user.email "cjtkirk@protonmail.com"
+
+echo "[INFO] Next: copy ~/.ssh/id_rsa.pub to github UI"
+  
